8b52b178291db5ad4990369a0ff9e6da2c5b5c07
razvan.bobes <razvan.bobes@stud.acs.upb.ro>
2026-01-14 21:07:32 +0200

    moddified Lexer, not done

--- src/Lexer.py
+++ src/Lexer.py
@@ -15,7 +15,7 @@ class Lexer:
 
         states = {self.q0}
         transitions = {}
-        final_states = {}
+        nfa_final_states = {}
         alphabet = set()
 
         offset = 1
@@ -31,29 +31,41 @@ class Lexer:
             alphabet |= nfa_remapped.S
 
             for fstate in nfa_remapped.F:
-                final_states[fstate] = (token_name, index)
+                nfa_final_states[fstate] = (token_name, index)
 
         new_nfa = NFA(
             S = alphabet,
             K = states,
             q0 = self.q0,
             d = transitions,
-            F = set(final_states.keys())
+            F = set(nfa_final_states.keys())
         )
 
-        self.final_states = final_states
-
         self.dfa = new_nfa.subset_construction()
-        self.dfa_min = self.dfa.minimize()
+        self.dfa_final_map = {}
+
+        for dfa_state in self.dfa.K:
+            best_prio = float('inf')
+            best_token = None
+
+            for nfa_state in dfa_state:
+                if nfa_state in nfa_final_states:
+                    token, prio = nfa_final_states[nfa_state]
+
+                    if prio < best_prio:
+                        best_prio = prio
+                        best_token = token
+
+            if best_token is not None:
+                self.dfa_final_map[dfa_state] = (best_token, best_prio)
     
     def lex(self, word: str) -> list[tuple[str, str]]:
-
         result = []
         i = 0
         line = 0
 
         while i < len(word):
-            current_state = self.dfa_min.q0
+            current_state = self.dfa.q0
 
             last_final_index = -1
             last_final_state = None
@@ -63,28 +75,36 @@ class Lexer:
             while j < len(word):
                 symbol = word[j]
 
-                if (current_state, symbol) not in self.dfa_min.d:
+                if (current_state, symbol) not in self.dfa.d:
                     break
 
-                current_state = self.dfa_min.d[(current_state, symbol)]
+                current_state = self.dfa.d[(current_state, symbol)]
 
-                if current_state in self.final_states:
+                if current_state in self.dfa_final_map:
                     last_final_index = j
                     last_final_state = current_state
                 
                 j += 1
             
             if last_final_index == -1:
-                if j == len(word):
-                    result.append(("", f"No viable alternative at character EOF, line {line}"))
+                if j == len(word) and i == j:
+                    if i == len(word):
+                        return []
+                    if j == len(word):
+                        return[("", f"No viable alternative at character EOF, line {line}")]
+                    else:
+                        return[("", f"No viable alternative at character {i}, line {line}")]
                 else:
-                    result.append(("", f"No viable alternative at character {i}, line {line}"))
-                i += 1
+                    if j == len(word):
+                        return [("", f"No viable alternative at character EOF, line {line}")]
+                    else:
+                        return [("", f"No viable alternative at character {i}, line {line}")]
             else:
-                token_name, _ = self.final_states[last_final_state]
+                token_name, _ = self.dfa_final_map[last_final_state]
                 token_value = word[i:last_final_index + 1]
 
                 result.append((token_name, token_value))
+                line += token_value.count('\n')
 
                 i = last_final_index + 1
 
